2207cda74d617204ce5265d50801eab8ff64bd4b
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-04 14:07:14 +0200

    staging

--- code/src/GoalPioneers/Formats/txt/TXTExporter.java
+++ code/src/GoalPioneers/Formats/txt/TXTExporter.java
@@ -1,6 +1,13 @@
 package GoalPioneers.Formats.txt;
 
+import GoalPioneers.Formats.Templates.DataSet.DatasetExporterFacade;
+
+
+/**
+ * 
+ */
 public class TXTExporter 
+        implements DatasetExporterFacade
 {
     public TXTExporter()
     {
--- code/src/GoalPioneers/Formats/txt/TXTImporter.java
+++ code/src/GoalPioneers/Formats/txt/TXTImporter.java
@@ -1,10 +1,14 @@
 package GoalPioneers.Formats.txt;
 
 
+import GoalPioneers.Formats.Templates.DataSet.DatasetImporterFacade;
+
+
 /**
  * 
  */
 public class TXTImporter 
+        implements DatasetImporterFacade
 {
     // Constructor
     public TXTImporter()
